af69c05b9824226be20a00a706e431d43d764fa9
Jens21 <jens-beisswenger@gmx.de>
2023-08-11 20:57:50 +0200

    Initial commit

--- src/jens/MuZero/main_evaluation.py
+++ src/jens/MuZero/main_evaluation.py
@@ -10,13 +10,13 @@ random.seed(51342)
 np.random.seed(12347)
 th.manual_seed(54323)
 
-RENDER = False
-GAMMA = 0.95
+RENDER = True
+GAMMA = 0.975
 TREE_SEARCH_DEPTH = 5
 
 def evaluate_single_checkpoint(checkpoint_path):
-    shared_memory = SharedMemory()
-    shared_memory.set_current_model(th.load(checkpoint_path))
+    shared_memory = SharedMemory.remote()
+    shared_memory.set_current_model.remote(th.load(checkpoint_path))
     evaluator = Evaluator(gamma=GAMMA, shared_memory=shared_memory, tree_search_depth=TREE_SEARCH_DEPTH)
 
     n_won, n_draw, n_lost, n_games, net = evaluator.evaluate(RENDER)
@@ -24,15 +24,5 @@ def evaluate_single_checkpoint(checkpoint_path):
                                                                     100 * n_draw / n_games, n_lost,
                                                                     100 * n_lost / n_games))
 
-def evaluate_all_checkpoints(checkpoints_dir):
-    files = os.listdir(checkpoints_dir)
-    n_files = len(files)
-
-    for i, file in enumerate(files):
-        print('Checkpoint: {} ({:.2f} %)'.format(file, i/n_files*100))
-        evaluate_single_checkpoint(os.path.join(checkpoints_dir, file))
-        print()
-
 if __name__ == '__main__':
-    # evaluate_single_checkpoint('checkpoints/agent_3800000.pth')
-    evaluate_all_checkpoints("checkpoints")
+    evaluate_single_checkpoint("muzero_final.pth")
